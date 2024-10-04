15c3d5e65b48ad05a64873df928a2ada7e3a7b55
wignn <tigfiragnafatur1933@gmail.com>
2024-10-04 19:08:39 +0700

    a

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -118,7 +118,7 @@ export const reset = async (req, res) => {
   <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px; background-color: #f9f9f9;">
     <h2 style="color: #333;">Password Reset Request</h2>
     <p>We received a request to reset your password. Click the button below to reset it.</p>
-    <a href="http://localhost:3000/${domain}/${token}" style="display: inline-block; padding: 10px 20px; margin: 20px 0; background-color: #007bff; color: #fff; text-decoration: none; border-radius: 5px;">Reset Your Password</a>
+    <a href="https://frontend-for-crud.vercel.app/reset/${token}" style="display: inline-block; padding: 10px 20px; margin: 20px 0; background-color: #007bff; color: #fff; text-decoration: none; border-radius: 5px;">Reset Your Password</a>
     <p>If you did not request a password reset, please ignore this email.</p>
     <p style="color: #888;">Thank you,<br/>Othinus</p>
   </div>
